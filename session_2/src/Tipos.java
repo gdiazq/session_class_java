9ca69dae4f6eb982b7bc1ead33df8750ad065065
Guillermo Diaz <guillermoe.diazq@gmail.com>
2024-09-18 01:41:23 -0300

    Sesiones de clase de Java V2-1

--- session_2/src/Tipos.java
+++ session_2/src/Tipos.java
@@ -13,5 +13,19 @@ public class Tipos {
         System.out.println("tipo short corresponde en bits a: " + Short.SIZE);
         System.out.println("valor minimo short: " + Short.MIN_VALUE);
         System.out.println("valor maximo short: " + Short.MAX_VALUE);
+
+        int numeroInt = 2147483647;
+        System.out.println("Numero int: " + numeroInt);
+        System.out.println("tipo int corresponde en byte a: " + Integer.BYTES);
+        System.out.println("tipo int corresponde en bits a: " + Integer.SIZE);
+        System.out.println("valor minimo int: " + Integer.MIN_VALUE);
+        System.out.println("valor maximo int: " + Integer.MAX_VALUE);
+
+        long numeroLong = 9223372036854775807L;
+        System.out.println("Numero long: " + numeroLong);
+        System.out.println("tipo long corresponde en byte a: " + Long.BYTES);
+        System.out.println("tipo long corresponde en bits a: " + Long.SIZE);
+        System.out.println("valor minimo long: " + Long.MIN_VALUE);
+        System.out.println("valor maximo long: " + Long.MAX_VALUE);
     }
 }
