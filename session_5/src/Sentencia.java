01dfc652adc855b2208a0e13f7e84bf0d6231648
Guillermo Diaz <guillermoe.diazq@gmail.com>
2024-09-20 13:12:46 -0300

    Sesiones de clase de Java V2-9

--- session_5/src/Sentencia.java
+++ session_5/src/Sentencia.java
@@ -4,6 +4,11 @@ public class Sentencia {
         int numero = 10;
         if (numero > 0) {
             System.out.println("El numero es positivo");
+            if (numero % 2 == 0) {
+                System.out.println("El numero es par");
+            } else {
+                System.out.println("El numero es impar");
+            }
         } else {
             System.out.println("El numero es negativo");
         }
