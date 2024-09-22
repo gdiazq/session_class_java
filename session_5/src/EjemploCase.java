2c661aadd1b43285145e6ccf12e9875752527dc9
Guillermo Diaz <guillermoe.diazq@gmail.com>
2024-09-22 18:50:26 -0300

    Sesiones de clase de Java V2-13

---
+++ session_5/src/EjemploCase.java
@@ -0,0 +1,28 @@
+public class EjemploCase {
+    public static void main(String[] args) {
+        System.out.println("Ejemplo de switch");
+        int numero = 10;
+        switch (numero) {
+            case 0:
+                System.out.println("El numero es 0");
+                break;
+            case 1:
+                System.out.println("El numero es 1");
+                break;
+            case 2:
+                System.out.println("El numero es 2");
+                break;
+            case 3:
+                System.out.println("El numero es 3");
+                break;
+            case 4:
+                System.out.println("El numero es 4");
+                break;
+            case 5:
+                System.out.println("El numero es 5");
+                break;
+            default:
+                System.out.println("El numero es mayor a 5");
+        }
+    }
+}
