f87f3e51171f35d60b19709511dfba479963ed11
Guillermo Diaz <guillermoe.diazq@gmail.com>
2024-09-22 18:48:25 -0300

    Sesiones de clase de Java V2-11

---
+++ session_5/src/BucleFor.java
@@ -0,0 +1,8 @@
+public class BucleFor {
+    public static void main(String[] args) {
+        System.out.println("Bucle for");
+        for (int i = 0; i < 5; i++) {
+            System.out.println("El valor de i es: " + i);
+        }
+    }
+}
