0ad30d1654a34c5fc2feb10313934ac0a4f6b78c
Guillermo Diaz <guillermoe.diazq@gmail.com>
2024-09-23 14:18:39 -0300

    Sesiones de clase de Java V2-15

---
+++ session_5/src/ForEach.java
@@ -0,0 +1,9 @@
+public class ForEach {
+    public static void main(String[] args) {
+        System.out.println("Ejemplo de for each");
+        int[] numeros = {1, 2, 3, 4, 5};
+        for (int numero : numeros) {
+            System.out.println("El numero es " + numero);
+        }
+    }
+}
